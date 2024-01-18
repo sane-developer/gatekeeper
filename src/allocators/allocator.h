1874a24330b68dcc4fe4c6973f2665215fb122b2
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-01-18 15:33:16 +0100

    Implemented generic interface for all allocators.

---
+++ src/allocators/allocator.h
@@ -0,0 +1,19 @@
+#ifndef GATEKEEPER_ALLOCATOR_H
+#define GATEKEEPER_ALLOCATOR_H
+
+#include <stddef.h>
+
+///
+/// @brief Allocates the memory chunk of requested size.
+/// @param size The size of the memory chunk.
+/// returns The pointer to the allocated memory block.
+///
+void* allocate_memory(size_t size);
+
+///
+/// @brief Frees the allocated memory.
+/// @param ptr The pointer to the allocated memory.
+///
+void free_memory(void* ptr);
+
+#endif
