b448f843500b971e42f8933cbe969a5e2ec9545a
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-01-18 15:35:04 +0100

    Placeholder for allocator that utilizes dirsrv API.

---
+++ src/allocators/slapd_allocator.c
@@ -0,0 +1,15 @@
+#include "allocator.h"
+
+void* allocate_memory(size_t size)
+{
+    // TODO: Implement using the dirsrv API.
+
+    return NULL;
+}
+
+void free_memory(void *ptr)
+{
+    // TODO: Implement using the dirsrv API.
+
+    return;
+}
