bebf3b768a745e8f0e88258a884aea8bb5089f5d
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-01-22 17:58:04 +0100

    Fixed compilation target printing in build.sh.

--- build.sh
+++ build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+BUILD_TYPE=""
+
 mkdir -p build
 
 cd build || exit 1
 
 if [ "$1" = "--debug" ] ; then
+  BUILD_TYPE="Debug"
   cmake -DCMAKE_BUILD_TYPE=Debug ..
 else
+  BUILD_TYPE="Release"
   cmake ..
 fi
 
 make
 
 if [ $? -eq 0 ]; then
-  echo "Debug build succeeded"
+  echo "$BUILD_TYPE build succeeded"
 else
-  echo "Debug build failed"
+  echo "$BUILD_TYPE build failed"
 fi
