8fbcf01bc1813cf3fe1acf4c1db4f9fd56432067
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-01-22 17:45:59 +0100

    Guard clauses for std_allocator.c.

--- src/allocators/allocator.h
+++ src/allocators/allocator.h
@@ -6,7 +6,7 @@
 ///
 /// @brief Allocates the memory chunk of requested size.
 /// @param size The size of the memory chunk.
-/// returns The pointer to the allocated memory block.
+/// @returns The pointer to the allocated memory block.
 ///
 void* allocate_memory(size_t size);
 
