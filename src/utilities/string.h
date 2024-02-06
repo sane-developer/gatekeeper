e4a20a7acded594b65115df42d642ec829719a46
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-02-06 17:16:09 +0100

    Reimplemented string API.

--- src/utilities/string.h
+++ src/utilities/string.h
@@ -4,7 +4,6 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../allocators/allocator.h"
 
 ///
 /// @brief Represents a mutable character buffer.
@@ -12,43 +11,29 @@
 typedef struct
 {
     char* item;
-    size_t capacity;
+    size_t size;
 }
 string_t;
 
 ///
 /// @brief Allocates an empty string on the heap.
-/// @param capacity The capacity of the string buffer.
+/// @param size The count of characters the buffer may store.
 /// @return The allocated string when allocated successfully; otherwise, NULL.
 ///
-string_t* string_from_size(size_t capacity);
+string_t* string_from_size(size_t size);
 
 ///
 /// @brief Allocates a string on the heap with contents from provided buffer.
-/// @param string The string containing contents of the newly allocated string.
+/// @param contents The contents of the newly allocated string.
 /// @return The allocated string when allocated successfully; otherwise, NULL.
 ///
-string_t* string_from_string(const string_t* string);
-
-///
-/// @brief Allocates a string on the heap with contents from provided buffer.
-/// @param buffer The contents of the newly allocated string.
-/// @return The allocated string when allocated successfully; otherwise, NULL.
-///
-string_t* string_from_string_literal(const char* buffer);
+string_t* string_from_contents(const char* contents);
 
 ///
 /// @brief Frees the contents of the string and the string itself.
 /// @param string The string to free.
 ///
 void dispose_string(string_t* string);
-
-///
-/// @brief Clears the contents of the string.
-/// @param string The string to clear.
-/// @return 1 when the string has been successfully cleared; otherwise, 0.
-///
-char clear_contents_of(const string_t* string);
 
 ///
 /// @brief Determines whether the string is empty.
@@ -63,18 +48,23 @@ char is_empty(const string_t* string);
 /// @param left The first string.
 /// @param right The second string.
 /// @return 1 when the contents of both strings are equal; otherwise 0.
-/// @remark 0 is also returned in case any of the strings or its buffer is NULL.
 ///
-char matches_contents_of_string(const string_t* left, const string_t* right);
+char matches_string(const string_t* left, const string_t* right);
 
 ///
 /// @brief Determines whether the strings are equal.
 /// @param left The string.
 /// @param right The string literal.
 /// @return 1 when the contents of both strings are equal; otherwise 0.
-/// @remark 0 is also returned in case the string or any of the buffers is NULL.
 ///
-char matches_contents_of_string_literal(const string_t* left, const char* right);
+char matches_string_literal(const string_t* left, const char* right);
+
+///
+/// @brief Clears the contents of the string.
+/// @param string The string to clear.
+/// @return 1 when the string has been successfully cleared; otherwise, 0.
+///
+void remove_contents(const string_t* string);
 
 ///
 /// @brief Moves the contents of one string to another.
@@ -84,7 +74,7 @@ char matches_contents_of_string_literal(const string_t* left, const char* right)
 /// @remark In case any of the strings or its buffer is NULL the operation is cancelled.
 /// @remark In case the moved contents are larger than the receiver's buffer capacity they will be trimmed to fit in.
 ///
-char yoink_contents_from_string(const string_t* src, string_t* dst);
+void yoink_string(const string_t* src, string_t* dst);
 
 ///
 /// @brief Moves the contents of one string to another.
@@ -94,7 +84,7 @@ char yoink_contents_from_string(const string_t* src, string_t* dst);
 /// @remark In case any of the strings or its buffer is NULL the operation is cancelled.
 /// @remark In case the moved contents are larger than the receiver's buffer capacity they will be trimmed to fit in.
 ///
-char yoink_contents_from_string_literal(const char* src, string_t* dst);
+void yoink_string_literal(const char* src, string_t* dst);
 
 ///
 /// @brief Removes all whitespaces from the string.
@@ -104,7 +94,7 @@ char yoink_contents_from_string_literal(const char* src, string_t* dst);
 /// @remark In case any of the strings or its buffer is NULL the operation is cancelled.
 /// @remark In case the moved contents are larger than the receiver's buffer capacity they will be trimmed to fit in.
 ///
-char erase_whitespaces_from_string(const string_t* src, string_t* dst);
+void erase_whitespaces_from_string(const string_t* src, string_t* dst);
 
 ///
 /// @brief Removes all whitespaces from the string literal.
@@ -114,6 +104,16 @@ char erase_whitespaces_from_string(const string_t* src, string_t* dst);
 /// @remark In case any of the strings or its buffer is NULL the operation is cancelled.
 /// @remark In case the moved contents are larger than the receiver's buffer capacity they will be trimmed to fit in.
 ///
-char erase_whitespaces_from_string_literal(const char* src, string_t* dst);
+void erase_whitespaces_from_string_literal(const char* src, string_t* dst);
+
+///
+/// @brief
+/// @param src
+/// @param key
+/// @param value
+/// @param delimiter
+/// @return
+///
+void split_by_delimiter(const string_t* src, string_t* key, string_t* value, char delimiter);
 
 #endif
