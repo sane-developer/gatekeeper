0766a526ea0425b42e28098adb9b3951ed40fea2
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-02-06 17:15:11 +0100

    Defined abstractions for validators. Additionally, implemented the logging policy validator.

---
+++ src/validators/binding_policy_validator.h
@@ -0,0 +1,13 @@
+#ifndef GATEKEEPER_BINDING_POLICY_VALIDATOR_H
+#define GATEKEEPER_BINDING_POLICY_VALIDATOR_H
+
+#include "../models/binding_policy.h"
+
+///
+/// @brief
+/// @param policy
+/// @return
+///
+char validate_binding_policy(const binding_policy_t* policy);
+
+#endif
