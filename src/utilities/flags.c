fcb41495bd4a662ada36192651f90be35a11ea5d
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-02-06 17:16:41 +0100

    Removed unnecessary constant declarations.

--- src/utilities/flags.c
+++ src/utilities/flags.c
@@ -1,18 +1,18 @@
 #include "flags.h"
 
-char has_flag(const int src, const int target)
+char has_flag(int src, int target)
 {
     return (src & target) == target;
 }
 
-void enable_flag(void* src, const int target)
+void enable_flag(void* src, int target)
 {
     int* flags = src;
 
     *flags |= target;
 }
 
-void disable_flag(void* src, const int target)
+void disable_flag(void* src, int target)
 {
     int* flags = src;
 
