8cd2fb36d96a3f471adca7d882c575eb3332fc1d
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-04-21 16:18:30 +0200

    Adjusted methods for new headers for bind request datetime methods.

--- api/requests/bind_request_datetime_t.c
+++ api/requests/bind_request_datetime_t.c
@@ -1,16 +1,16 @@
 #include "bind_request_datetime_t.h"
 
-int set_full_date()
+int set_full_date(const struct tm* local_time, char* full_date)
 {
     return 1;
 }
 
-int set_day_of_week()
+int set_day_of_week(const struct tm* local_time, uint32_t* day_of_week)
 {
     return 1;
 }
 
-int set_time_of_day()
+int set_time_of_day(const struct tm* local_time, uint32_t* time_of_day)
 {
     return 1;
 }
