7b380531b5c71b18a5db4528f5793aca747e5333
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-04-22 15:23:16 +0200

    Skeleton for bind request initialization procedure.

--- api/requests/bind_request_client_t.c
+++ api/requests/bind_request_client_t.c
@@ -10,22 +10,52 @@ static const char stringified_auth_methods[6][16] =
     [SASL_DIGEST_MD5] = "sasl digest-md5"
 };
 
-int set_client_dn(Slapi_PBlock* block, char* dn)
+static int set_client_dn(const Slapi_PBlock* const block, bind_request_client_t* const client)
 {
     return 1;
 }
 
-int set_client_ip(Slapi_PBlock* block, uint32_t* ip)
+static int set_client_ip(const Slapi_PBlock* const block, bind_request_client_t* const client)
 {
     return 1;
 }
 
-int set_client_dns(Slapi_PBlock* block, uint32_t* dns)
+static int set_client_dns(const Slapi_PBlock* const block, bind_request_client_t* const client)
 {
     return 1;
 }
 
-int set_client_auth(Slapi_PBlock* block, authentication_method_t* auth)
+static int set_client_auth(const Slapi_PBlock* const block, bind_request_client_t* const client)
 {
     return 1;
+}
+
+int initialize_bind_request_client(const Slapi_PBlock* const block, bind_request_client_t* const client)
+{
+    if (!set_client_dn(block, client))
+    {
+        return 0;
+    }
+
+    if (!set_client_ip(block, client))
+    {
+        return 0;
+    }
+
+    if (!set_client_dns(block, client))
+    {
+        return 0;
+    }
+
+    if (!set_client_auth(block, client))
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+void dispose_bind_request_client(bind_request_client_t* client)
+{
+    (void) client;
 }
--- api/requests/bind_request_t.c
+++ api/requests/bind_request_t.c
@@ -1,73 +1,32 @@
 #include "bind_request_t.h"
 
-int setup_bind_request(Slapi_PBlock* block, bind_request_t* const request)
+int initialize_bind_request(Slapi_PBlock* block, bind_request_t* const request)
 {
-    char* client_dn;
+    bind_request_datetime_t datetime = {0};
 
-    if (!set_client_dn(block, client_dn))
+    initialize_bind_request_datetime(&datetime); 
+
+    bind_request_client_t client = {0};
+
+    if (!initialize_bind_request_client(block, &client))
     {
-        return 0;
-    }
-
-    uint32_t client_ip;
-
-    if (!set_client_ip(block, &client_ip))
-    {
-        return 0;
-    }
-
-    uint32_t client_dns;
-
-    if (!set_client_dns(block, &client_dns))
-    {
-        return 0;
-    }
-
-    authentication_method_t client_auth;
-
-    if (!set_client_auth(block, &client_auth))
-    {
+        dispose_bind_request_client(&client);
         return 0;
     }
 
     *request->client = (bind_request_client_t)
     {
-        .dn = client_dn,
-        .ip = client_ip,
-        .dns = client_dns,
-        .auth = client_auth
+        .dn = client.dn,
+        .ip = client.ip,
+        .dns = client.dns,
+        .auth = client.auth
     };
-
-    time_t current_time = time(NULL);
-
-    struct tm* local_time = localtime(&current_time);
-
-    char* request_full_date;
-
-    if (!set_full_date(local_time, request_full_date))
-    {
-        return 1;
-    }
-
-    uint32_t request_day_of_week;
-
-    if (!set_day_of_week(local_time, &request_day_of_week))
-    {
-        return 0;
-    }
-
-    uint32_t request_time_of_day;
-
-    if (!set_time_of_day(local_time, &request_time_of_day))
-    {
-        return 0;
-    }
 
     *request->datetime = (bind_request_datetime_t)
     {
-        .full_date = request_full_date,
-        .day_of_week = request_day_of_week,
-        .time_of_day = request_time_of_day
+        .full_date = datetime.full_date,
+        .day_of_week = datetime.day_of_week,
+        .time_of_day = datetime.time_of_day
     };
 
     return 1;
@@ -80,13 +39,8 @@ void dispose_bind_request(bind_request_t* request)
         return;
     }
 
-    if (request->client->dn)
+    if (request->client)
     {
-        // TODO: Free the client dn.
-    }
-
-    if (request->datetime->full_date)
-    {
-        // TODO: Free the full date.
+        dispose_bind_request_client(request->client);
     }
 }
