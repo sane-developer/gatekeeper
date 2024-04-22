3bb4d800d0fa9ed3ddf1b141d11ac4e49ec89a2c
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-04-22 23:53:09 +0200

    Removed bind_request_status_t structure overhead.

--- api/bind_request_client.h
+++ api/bind_request_client.h
@@ -30,10 +30,10 @@ bind_request_client_t;
 
 ///
 /// @brief Determines whether the specified DN matches the one of the client.
-/// 
+///
 /// @param client Pointer to a bind_request_client_t variable that stores the information about LDAP client.
 /// @param dn Pointer to a string that contains a DN to match against.
-/// 
+///
 /// @return
 ///     - 1, when the DNs are equal.
 ///     - 0, otherwise.
@@ -42,10 +42,10 @@ int matches_client_dn(const bind_request_client_t* const client, const char* con
 
 ///
 /// @brief Determines whether the LDAP client belongs to the specified role.
-/// 
+///
 /// @param client Pointer to a bind_request_client_t variable that stores the information about LDAP client.
 /// @param role_name Pointer to a string that contains the name of the role to check against.
-/// 
+///
 /// @return
 ///     - 1, when the LDAP client belongs to the specified role.
 ///     - 0, otherwise.
@@ -54,22 +54,22 @@ int matches_client_roles(const bind_request_client_t* const client, const char* 
 
 ///
 /// @brief Determines whether the LDAP client belongs to the specified group.
-/// 
+///
 /// @param client Pointer to a bind_request_client_t variable that stores the information about LDAP client.
 /// @param group_name Pointer to a string that contains the name of the group to check against.
-/// 
+///
 /// @return
 ///     - 1, when the LDAP client belongs to the specified group.
-///     - 0, otherwise .
+///     - 0, otherwise.
 ///
 int matches_client_groups(const bind_request_client_t* const client, const char* const group_name);
 
 ///
 /// @brief Determines whether the LDAP client has the specified attribute assigned.
-/// 
+///
 /// @param client Pointer to a bind_request_client_t variable that stores the information about LDAP client.
 /// @param attribute Pointer to a string that contains the attribute to check against.
-/// 
+///
 /// @return
 ///     - 1, when the LDAP client has the specified attribute assigned.
 ///     - 0, otherwise.
