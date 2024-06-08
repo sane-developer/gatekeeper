c91f0d4b96bb31a488780c14f78a035de00e569f
sane-developer <krzysztof.wodzinski.me@gmail.com>
2024-06-09 00:00:47 +0200

    Removed bind request evaluator unit tests.

--- core/tests/shared/variables.c
+++
@@ -1,9 +0,0 @@
-#include "plugin.h"
-
-aci_rules_t plugin_deny_aci_rules = {0};
-
-aci_rules_t plugin_grant_aci_rules = {0};
-
-Slapi_ComponentId* plugin_component_identity = NULL;
-
-Slapi_PluginDesc plugin_component_description = {0};
